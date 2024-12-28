6f34ee30c936949c954e9567da51080b155da27e
manimohan <manimohan.t22@gmail.com>
2024-12-28 19:38:48 +0530

    created assignment script

--- assignment.sh
+++ assignment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 FILE=$1
-COUNT=$(wc $FILE)
-echo "Word count in file $FILE is:: $COUNT"
+WORD_COUNT=$(wc -w $FILE)
+LINE_COUNT=$(wc -l $FILE)
+CHAR_COUNT=$(wc -c $FILE)
+EACH_WORD=$(tr -cs '[:alnum:]' '[\n*]' < $FILE | sort | uniq -c | sort -nr | head -n 5)
+echo "Word count in file $FILE is:: $WORD_COUNT"
+echo "Line count in file $FILE is:: $LINE_COUNT"
+echo "Character count in file $FILE is:: $CHAR_COUNT"
+echo "Occurrence of each word & Top 5 words:: $EACH_WORD"
